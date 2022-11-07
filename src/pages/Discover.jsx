3ec3573ec4eb22ea565ba71c9ba5fec636f05093
rakeshwasnik <rbwasnik99@gmail.com>
2022-11-07 19:33:59 +0530

    .

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,15 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+
+    console.log(genres);
+    
+    return(
+        <div>Discover</div>
+    );
+}
+
+
 
 export default Discover;
