e77450e1047c47acbc2d6986c8b57f9f58007730
rakeshwasnik <rbwasnik99@gmail.com>
2022-11-09 19:13:37 +0530

    .

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,4 +14,12 @@ const options = {
     .then(response => console.log(response))
     .catch(err => console.error(err));
 
-    export const shazamCoreApi = 
+    export const shazamCoreApi = createApi({
+        reducerPath: 'shazamCoreApi',
+        baseQuery: fetchBaseQuery({
+            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+            prepareHeaders: () => {
+                headers.set('X-RapidAPI-Key', '38dc50287bmsh4bed387d626f116p1f14c1jsnc532a124b9cb')
+            }
+        })
+    });
