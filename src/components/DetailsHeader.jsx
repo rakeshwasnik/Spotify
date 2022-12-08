a344174279c39b3c72e8b72707d9cf4f0a030d94
rakeshwasnik <rbwasnik99@gmail.com>
2022-12-08 17:20:05 +0530

    .

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,9 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
+import { Link } from "react-router-dom";
+
+const DetailsHeader = ({ artistId, artistData, songData }) => (
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+  </div>
 );
 
 export default DetailsHeader;
